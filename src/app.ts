ef9d05d3192ec528408e67848a858df3c273abb9
IlhamArbani <43053171+IlhamArbani@users.noreply.github.com>
2024-03-19 12:56:41 +0700

    feat: handel cors origin

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Express } from "express";
 import { usersRouter, loginRouter, productsRouter } from "./routes";
 import { errorHandler } from "@utils/errorHandler";
+import cors from 'cors';
 
 const app: Express = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(usersRouter);
 app.use(productsRouter);
